9c96fc3e134c33844a8b0614055dad30461a190d
ibrahim_ahmad <ibrahimahmad.info@gmail.com>
2024-03-18 10:28:31 +0800

    Add function to check winner based on row pattern.

--- ticTacToe.js
+++ ticTacToe.js
@@ -26,7 +26,46 @@ function gameBoard(boardArray) {
     const getBoard = () => {console.log(boardArray)};
 
     
-    return {playerSelect, getBoard};
+    // check row
+    const checkRow = () =>{
+        
+        let result_player1=[];
+        let result_player2=[];
+
+        for (let row=0; row<3; row++){
+            
+            // initialize counter at start of each row
+            let counter_player1 = 0;
+            let counter_player2 = 0;
+
+            // count element at each column
+            for (let col=0; col<3; col++){
+                cellValue = boardArray[row][col];
+
+                if (cellValue === 'x'){counter_player1++};
+                if (cellValue === 'o'){counter_player2++}
+            };
+
+            // declare winner
+            if (counter_player1 === 3){
+                console.log(`Player1 wins in row ${row+1}.`);
+                break;
+            };
+            
+            if (counter_player2 === 3){
+                console.log(`Player2 wins in row ${row+1}.`);
+                break;
+            };
+
+            if (row === 2){console.log('Row check completed. No winner to declare.')};
+            
+        };
+        
+    };
+
+
+
+    return {playerSelect, getBoard, checkRow};
 
 };
 
@@ -35,3 +74,10 @@ let board1 = [['_','_','_'], ['_','_','_'], ['_','_','_']];
 const game1 = gameBoard(board1);
 game1.playerSelect('1-2','x');
 game1.playerSelect('1-1','o');
+game1.playerSelect('1-3','o');
+game1.checkRow();
+
+game1.playerSelect('2-1','o');
+game1.playerSelect('2-2','o');
+game1.playerSelect('2-3','o');
+game1.checkRow();
