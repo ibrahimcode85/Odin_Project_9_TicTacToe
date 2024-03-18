48d512501159f51aac86d00c79b0b041bb86ba50
ibrahim_ahmad <ibrahimahmad.info@gmail.com>
2024-03-18 15:44:07 +0800

    Add function to check winner based on column pattern.

--- ticTacToe.js
+++ ticTacToe.js
@@ -29,21 +29,18 @@ function gameBoard(boardArray) {
     // check row
     const checkRow = () =>{
         
-        let result_player1=[];
-        let result_player2=[];
-
-        for (let row=0; row<3; row++){
+        for (let row = 0; row <3 ; row++){
             
             // initialize counter at start of each row
             let counter_player1 = 0;
             let counter_player2 = 0;
 
             // count element at each column
-            for (let col=0; col<3; col++){
+            for (let col = 0; col <3; col++){
                 cellValue = boardArray[row][col];
 
                 if (cellValue === 'x'){counter_player1++};
-                if (cellValue === 'o'){counter_player2++}
+                if (cellValue === 'o'){counter_player2++};
             };
 
             // declare winner
@@ -58,20 +55,50 @@ function gameBoard(boardArray) {
             };
 
             if (row === 2){console.log('Row check completed. No winner to declare.')};
-            
-        };
+        }; 
+    }; // end of check row
+
+    const checkCol = () =>{
+
+        for (let col = 0; col <3 ; col++){
+
+            // initialize counter at start of each row
+            let counter_player1 = 0;
+            let counter_player2 = 0;
+
+            for (let row = 0; row < 3; row++){
+                cellValue = boardArray[row][col];
+
+                if (cellValue === 'x'){counter_player1++};
+                if (cellValue === 'o'){counter_player2++};
+            };
         
-    };
+            // declare winner
+            if (counter_player1 === 3){
+                console.log(`Player1 wins in col ${col + 1}.`);
+                break;
+            };
+            
+            if (counter_player2 === 3){
+                console.log(`Player2 wins in col ${col + 1}.`);
+                break;
+            };
 
+            if (col === 2){console.log('Col check completed. No winner to declare.')};
+        };
+    }; //end of colCheck
 
-
-    return {playerSelect, getBoard, checkRow};
+    return {playerSelect, getBoard, checkRow, checkCol};
 
 };
 
 // run the game
-let board1 = [['_','_','_'], ['_','_','_'], ['_','_','_']];
-const game1 = gameBoard(board1);
+let initBoard = [['_','_','_'], ['_','_','_'], ['_','_','_']];
+let initBoard2 = [['_','_','_'], ['_','_','_'], ['_','_','_']];
+const game1 = gameBoard(initBoard);
+const game2 = gameBoard(initBoard2);
+
+// row pattern
 game1.playerSelect('1-2','x');
 game1.playerSelect('1-1','o');
 game1.playerSelect('1-3','o');
@@ -81,3 +108,14 @@ game1.playerSelect('2-1','o');
 game1.playerSelect('2-2','o');
 game1.playerSelect('2-3','o');
 game1.checkRow();
+
+// column pattern
+game2.playerSelect('1-2','x');
+game2.playerSelect('2-2','o');
+game2.playerSelect('3-2','o');
+game2.checkCol();
+
+game2.playerSelect('1-1','x');
+game2.playerSelect('2-1','x');
+game2.playerSelect('3-1','x');
+game2.checkCol();
